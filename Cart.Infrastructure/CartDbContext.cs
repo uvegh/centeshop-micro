50842e2d1f95f2211a251cc4faaefbaed0739efc
uevgh <uveghobamien@gmail.com>
2025-11-14 17:16:25 +0000

    Add EF Core and in-memory repository for Cart service
    Updated Cart.Infrastructure.csproj to target .NET 8.0, enable nullable reference types, and add EF Core and PostgreSQL dependencies. Introduced `CartDbContext` for database context setup and `InMemoryCartRepository` for temporary in-memory cart storage. These changes establish the foundational infrastructure for the Cart service.

---
+++ Cart.Infrastructure/CartDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Cart.Infrastructure;
+using Cart.Domain.Entities;
+
+public  class CartDbContext:DbContext
+{
+
+
+    public CartDbContext(DbContextOptions options): base(options)
+    {
+
+    }
+
+    public DbSet<Cart> Carts;
+    
+
+
+}
---
+++ Cart.Infrastructure/Repository/InMemoryCartRepository.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Cart.Domain.IRepository;
+using CartEntity = Cart.Domain.Entities.Cart;
+namespace Cart.Infrastructure.Repository;
+
+public  class InMemoryCartRepository:ICartRespository
+{
+    //temporary in memorydb for carts
+    private readonly Dictionary<Guid, CartEntity> _carts = new();
+   
+public Task<CartEntity?> GetByUserIdAsync(Guid UserId)
+    {
+        _carts.TryGetValue(UserId, out var cart);
+        if (cart != null){
+            Task.FromResult(cart);
+        }
+        return null;
+       
+      
+    }
+
+    public Task SaveAsync(CartEntity cart)
+    {
+
+        
+        _carts[cart.UserId] = cart;
+        return Task.CompletedTask;
+    }
+}
