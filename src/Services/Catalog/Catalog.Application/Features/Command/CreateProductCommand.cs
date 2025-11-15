78f01637494e9888382816d79f793d25097fe067
uevgh <uveghobamien@gmail.com>
2025-11-15 23:54:02 +0000

    Add MediatR and implement CreateProductCommand
    Added the MediatR package (v13.1.0) to `Catalog.Application.csproj` and introduced a folder structure for `Features\Command\Product\`.
    
    Created `CreateProductCommand` as a record implementing `IRequest` to handle product creation. Added `CreateProductCommandHandler` to process the command, including database operations via `CatalogDbContext` and logging with `ILogger`.

---
+++ src/Services/Catalog/Catalog.Application/Features/Command/CreateProductCommand.cs
@@ -0,0 +1,5 @@
+﻿using MediatR;
+
+namespace Catalog.Application.Features.Command;
+
+public  record  CreateProductCommand( string Name, decimal Price, int StockQuantity) :IRequest;
---
+++ src/Services/Catalog/Catalog.Application/Features/Command/CreateProductCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿
+
+using ProductEntity = Catalog.Domain.Entities.Product;
+using Catalog.Infrastructure.Data;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+
+namespace Catalog.Application.Features.Command;
+
+public class CreateProductCommandHandler:IRequestHandler<CreateProductCommand>
+{
+    private readonly CatalogDbContext _dbContext;
+    private readonly ILogger _logger;
+    public CreateProductCommandHandler(CatalogDbContext dbContext, ILogger<CreateProductCommandHandler> logger)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+    }
+
+    public async Task Handle(CreateProductCommand req, CancellationToken ct)
+    {
+        var newProduct = ProductEntity.Create(req.Name, req.Price, req.StockQuantity);
+
+         _dbContext. Add(newProduct);
+      
+        await _dbContext.SaveChangesAsync(ct);
+        _logger.LogInformation("add new Product {newProduct}", newProduct);
+    
+
+
+    }
+}
