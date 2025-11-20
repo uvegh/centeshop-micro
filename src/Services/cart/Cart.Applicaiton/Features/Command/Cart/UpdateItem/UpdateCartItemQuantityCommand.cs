5ab02a4cd1fba3fc3be8b5f8b00f7d71b329afe8
uevgh <uveghobamien@gmail.com>
2025-11-20 11:34:02 +0000

    Add command and handler for updating cart item quantity
    Introduced `UpdateCartItemQuantityCommand` to encapsulate the
    logic for updating the quantity of a specific item in a user's
    cart. Added `UpdateCartItemQuantityCommandHandler` to process
    the command, leveraging `ICartRepository` for data access and
    `ILogger` for logging.
    
    The handler retrieves the user's cart, checks for the existence
    of the specified product, updates its quantity, and returns the
    updated cart item.

---
+++ src/Services/cart/Cart.Applicaiton/Features/Command/Cart/UpdateItem/UpdateCartItemQuantityCommand.cs
@@ -0,0 +1,6 @@
+﻿using Cart.Domain.Entities;
+
+namespace Cart.Application.Features.Command.Cart.UpdateItem;
+
+public record  UpdateCartItemQuantityCommand(Guid UserId, Guid ProductId, int Quantity):IRequest<CartItem>;
+
---
+++ src/Services/cart/Cart.Applicaiton/Features/Command/Cart/UpdateItem/UpdateCartItemQuantityCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿using Cart.Domain.Entities;
+using Cart.Domain.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cart.Application.Features.Command.Cart.UpdateItem
+{
+    public class UpdateCartItemQuantityCommandHandler:IRequestHandler<UpdateCartItemQuantityCommand,CartEntity>
+    {
+        private readonly ILogger _logger;
+        private readonly ICartRespository _cartRepo; 
+        public UpdateCartItemQuantityCommandHandler(ILogger<UpdateCartItemQuantityCommandHandler> logger, ICartRespository cartRepo)
+        {
+            _logger = logger;
+            _cartRepo = cartRepo;
+            
+        }
+
+        public async Task<CartItem> Handle(UpdateCartItemQuantityCommand req ,CancellationToken ct)
+        {
+            var cart = await _cartRepo.GetByUserIdAsync(req.UserId);
+            if (cart != null)
+            {
+            var item=    cart.Items.FirstOrDefault(x => x.ProductId == req.ProductId);
+
+                if (item != null)
+                {
+                    item.UpdateQuantity(req.Quantity);
+
+                    await _cartRepo.
+                    return item;
+
+                }
+                
+            }
+            
+
+
+        }
+    }
+}
